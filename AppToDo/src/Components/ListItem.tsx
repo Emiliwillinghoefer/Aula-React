8414786285433ad5b8b8b7664155cc7b009a6251
emiliwillinghoefer <emili_willi@hotmail.com>
2025-06-27 19:02:49 -0300

    Aula 7 - provider e context

--- AppToDo/src/Components/ListItem.tsx
+++ AppToDo/src/Components/ListItem.tsx
@@ -1,36 +1,78 @@
 import type {ToDo} from "../Models/ToDo.ts";
 import {Trash} from "phosphor-react";
 import style from "../Styles/listItem.module.css";
-import type {PropsWithChildren} from "react";
+import {createContext, type PropsWithChildren, useContext} from "react";
 
 interface ListItemProps extends ToDo {
     onApagarClick: () => void;
     onCheckBoxClick: (check:boolean) => void;
 }
 
+interface ListItemPropsContext extends ListItemProps {}
+
+const ListItemContext = createContext<ListItemPropsContext | undefined>( undefined );
+
+function useListItems() {
+    const context =  useContext(ListItemContext);
+    if (!context) {
+        throw new Error("Nenhum provider de ListItem");
+    }
+    return context;
+}
+
+function ListItemProvider(props: PropsWithChildren<{ value: ListItemProps }>) {
+    return <ListItemContext value={props.value}>
+        {props.children}
+    </ListItemContext>
+}
+
 export function ListItem(props: ListItemProps) {
     return (
-        <div className={style.listItem}>
-            <input onChange={
-                event => {
-                    props.onCheckBoxClick(event.target.checked)
-                }
-            } type={"checkbox"} />
-            <Text checked={props.checked}>
-                {props.text}
-            </Text>
-            <button onClick={props.onApagarClick}>
-                <Trash size={24}/>
-            </button>
-        </div>
+        <ListItemProvider value={props}>
+        <ListItemContent/>
+        </ListItemProvider>
+
     )
 }
 
 
-function Text(prop: PropsWithChildren<{checked:boolean}>) {
+function ListItemContent() {
     return (
-        <p className={ prop.checked ? style.textoDecorado : "" }>
-            {prop.children}
+        <div className={style.listItem}>
+            <ListItemCheckbox/>
+            <ListItemText />
+            <ListItemDeleteButton/>
+        </div>
+    )
+}
+
+function ListItemDeleteButton() {
+    const {onApagarClick} = useListItems();
+    return (
+        <button onClick={onApagarClick}>
+            <Trash size={24}/>
+        </button>
+    )
+}
+
+function ListItemCheckbox() {
+
+    const {onCheckBoxClick} = useListItems();
+    return (
+        <input onChange={
+            event => {
+                onCheckBoxClick(event.target.checked)
+            }
+        } type={"checkbox"} />
+    )
+}
+
+
+function ListItemText() {
+    const{checked, text} = useListItems();
+    return (
+        <p className={checked ? style.textoDecorado : "" }>
+            {text}
         </p>
 
     )
