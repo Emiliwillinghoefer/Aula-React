8a8de17141ab242379ec40796c4c30705bd51588
emiliwillinghoefer <emili_willi@hotmail.com>
2025-07-01 19:08:04 -0300

    Aula sobre UseEffect

--- CronometroETemporizador/src/pages/cronometro.tsx
+++ CronometroETemporizador/src/pages/cronometro.tsx
@@ -1,3 +1,47 @@
+import {useEffect, useState} from "react";
+
 export function Cronometro() {
-  return <div>Cronometro</div>;
+
+  const [tempo, setTempo] = useState(0);
+  const [executando, setExecutando] = useState(false);
+
+  function onIniciarClick() {
+    setExecutando(true);
+  }
+
+  function onPausarClick() {
+    setExecutando(false);
+  }
+
+  function onResetarClick() {
+    setTempo(0);
+    setExecutando(false);
+  }
+
+  useEffect(() => {
+    if (executando) {
+      const idIntervalo = setInterval(
+          () =>
+              setTempo(valor => valor + 1), 1);
+
+      return () => clearInterval(idIntervalo);
+    }
+  }, [executando]);
+
+
+  return (
+      <div>
+        <p>{tempo}</p>
+        <button onClick={onIniciarClick}>Iniciar</button>
+        <button onClick={onPausarClick}>Pausar</button>
+        <button onClick={onResetarClick}>Resetar</button>
+      </div>
+  );
 }
+
+// ToDo : Função pausar.
+// TODO: Tempo 00:00.00
+// TODO: Estilizar
+
+//TODO: Gravar tempo (LAP)   -----> https://quick-time-spark.lovable.app/
+
