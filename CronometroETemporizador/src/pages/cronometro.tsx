1d2acc9a2cd4e54d097d459d58eaede7b4412c51
emiliwillinghoefer <emili_willi@hotmail.com>
2025-07-04 18:34:10 -0300

    Aula criando contextos para parar de renderizar todos os componentes.

--- CronometroETemporizador/src/pages/cronometro.tsx
+++ CronometroETemporizador/src/pages/cronometro.tsx
@@ -1,51 +1,102 @@
-import {useEffect, useState} from "react";
+import {createContext, type PropsWithChildren, useContext, useEffect, useMemo, useState} from "react";
+
+interface CronometroAPI{
+    iniciar(): void;
+    pausar(): void;
+    resetar(): void;
+    lap(tempo: number): void;
+}
+
+const CronometroAPI = createContext<CronometroAPI>({} as CronometroAPI);
+const CronometroTempo = createContext<number>(0);
+const CronometroExecutando = createContext<boolean>(false);
+
+const useCronometroAPI = () => useContext(CronometroAPI);
+const useCronometroTempo = () => useContext(CronometroTempo);
+const useCronometroExecutando = () => useContext(CronometroExecutando);
+
+function CronometroProvider(props: PropsWithChildren) {
+    const [tempo, setTempo] = useState(0);
+    const [executando, setExecutando] = useState(false);
+
+    const [laps, setLap] = useState<{ time: number }[]>([]);
+
+    const api = useMemo(() => {
+        function iniciar() {
+            setExecutando(true);
+        }
+
+        function pausar() {
+            setExecutando(false);
+        }
+
+        function resetar() {
+            setTempo(0);
+            setExecutando(false);
+        }
+
+        function lap(time: number) {
+            const newLap = {time};
+            setLap([...laps, newLap]);
+        }
+
+        return {iniciar, pausar, lap, resetar};
+    }, []);
+
+    useEffect(() => {
+        if (executando) {
+            const idIntervalo = setInterval(
+                () =>
+                    setTempo(valor => valor + 10), 10);
+
+            return () => clearInterval(idIntervalo);
+        }
+    }, [executando]);
+
+    return (
+        <CronometroAPI value={api}>
+            <CronometroExecutando value={executando}>
+                <CronometroTempo value={tempo}>
+                    {props.children}
+                </CronometroTempo>
+            </CronometroExecutando>
+        </CronometroAPI>
+    )
+}
 
 export function Cronometro() {
+    return (
+        <CronometroProvider>
+            <CronometroContent>
+            </CronometroContent>
+        </CronometroProvider>
+    )
+}
 
-  const [tempo, setTempo] = useState(0);
-  const [executando, setExecutando] = useState(false);
+export function CronometroContent() {
 
-  function onIniciarClick() {
-    setExecutando(true);
-  }
+    const executando = useCronometroExecutando();
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-black">
+            <div className="bg-[#121215] p-10 w-80  rounded shadow-lg flex flex-col items-center space-y-6">
+                <Titulo/>
+                <Tempo/>
+                <div className="flex justify-center gap-4 mt-4">
 
-  function onPausarClick() {
-    setExecutando(false);
-  }
+                    {executando
+                        ? <BotaoPausar />
+                        : <BotaoIniciar />
+                    }
 
-  function onResetarClick() {
-    setTempo(0);
-    setExecutando(false);
-  }
-
-  useEffect(() => {
-    if (executando) {
-      const idIntervalo = setInterval(
-          () =>
-              setTempo(valor => valor + 10), 10);
-
-      return () => clearInterval(idIntervalo);
-    }
-  }, [executando]);
-
-
-  return (
-
-      <div className="flex flex-col items-center justify-center h-screen bg-black">
-          <div className="bg-[#121215] p-10 w-80  rounded shadow-lg flex flex-col items-center space-y-6">
-              <Titulo/>
-              <Tempo tempo={tempo}/>
-              <div className="flex justify-center gap-4 mt-4">
-
-              {executando
-                  ? <BotaoPausar onPausarClick={onPausarClick}/>
-                  : <BotaoIniciar onIniciarClick={onIniciarClick}/>
-              }
-              <BotaoResetar onResetarClick={onResetarClick}/>
-              </div>
-          </div>
-      </div>
-  );
+                    {executando
+                        ? <BotaoLap />
+                        : null
+                    }
+                    <BotaoResetar/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 function getCalculoTempo(totalMs: number) {
@@ -60,39 +111,54 @@ function getCalculoTempo(totalMs: number) {
     return `${minStr}:${segStr}.${centStr}`;
 }
 
-function Tempo(props: { tempo: number }) {
+function Tempo() {
+    const tempo = useCronometroTempo();
     return (
         <div className="bg-black shadow shadow-purple-400 px-6 py-3 rounded">
             <p className="text-white text-3xl font-mono flex items-center justify-center">
-                {getCalculoTempo(props.tempo)}
+                {getCalculoTempo(tempo)}
             </p>
         </div>
     );
 }
 
-function BotaoIniciar(props:{onIniciarClick:() => void}) {
+function BotaoIniciar() {
+    const api = useCronometroAPI();
     return (
         <div className="border-0 bg-purple-600 rounded p-2 text-white">
-        <button
-            onClick={props.onIniciarClick}>
-            Iniciar
-        </button>
+            <button
+                onClick={api.iniciar}>
+                Iniciar
+            </button>
         </div>
     )
 }
 
-function BotaoPausar(props:{onPausarClick:() => void}) {
-    return(
+function BotaoPausar() {
+    const api = useCronometroAPI();
+
+    return (
         <div className="border-0 bg-purple-600 rounded p-2 text-white">
-        <button onClick={props.onPausarClick}>Pausar</button>
+            <button onClick={api.pausar}>Pausar</button>
         </div>
     )
 }
 
-function BotaoResetar(props:{onResetarClick:() => void}) {
+function BotaoResetar() {
+    const api = useCronometroAPI();
     return (
         <div className="border-0 bg-black rounded p-2 text-white">
-        <button onClick={props.onResetarClick}>Resetar</button>
+            <button onClick={api.resetar}>Resetar</button>
+        </div>
+    )
+}
+
+function BotaoLap() {
+    const api = useCronometroAPI();
+    const tempo = useCronometroTempo();
+    return (
+        <div className="border-0 bg-black rounded p-2 text-white">
+            <button onClick={() => api.lap(tempo)}>Lap</button>
         </div>
     )
 }
@@ -104,4 +170,6 @@ function Titulo() {
 }
 
 //TODO: Gravar tempo (LAP)   -----> https://quick-time-spark.lovable.app/
+//TODO: Separar os arquivos
+// Criar a tela de temporizador
 
