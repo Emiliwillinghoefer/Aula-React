da8a0182550e2ca737e6413c3355fd502e38e511
emiliwillinghoefer <emili_willi@hotmail.com>
2025-07-29 17:43:21 -0300

    estilizacao

--- AppToDo/src/Components/DeleteDialog.tsx
+++ AppToDo/src/Components/DeleteDialog.tsx
@@ -1,39 +1,37 @@
-import { forwardRef } from "react";
+import {type RefObject} from "react";
 
 interface DeleteDialogProps {
+    ref: RefObject<HTMLDialogElement>;
     simClick: () => void;
     naoClick: () => void;
 }
 
-export const DeleteDialog = forwardRef<HTMLDialogElement, DeleteDialogProps>(
-    ({ simClick, naoClick }, ref) => {
-        return (
-            <dialog
-                ref={ref}
-                className="open:flex open:items-center open:justify-center w-full h-full p-0 border-none rounded-xl
+export function DeleteDialog(props: DeleteDialogProps) {
+    return (
+        <dialog
+            ref={props.ref}
+            className="open:flex open:items-center open:justify-center w-full h-full p-0 border-none rounded-xl
              backdrop:bg-black/30 backdrop-blur-sm bg-transparent"            >
-                <div className="bg-white rounded-xl shadow-xl p-6 max-w-sm w-full text-center">
-                    <h2 className="text-xl font-semibold text-gray-800 mb-6">
-                        Tem certeza que deseja apagar?
-                    </h2>
-                    <div className="flex justify-center gap-4">
-                        <button
-                            onClick={simClick}
-                            className={"button"}
-                        >
-                            Sim
-                        </button>
-                        <button
-                            onClick={naoClick}
-                            className={"button"}
-                        >
-                            Não
-                        </button>
-                    </div>
+            <div className="bg-white rounded-xl shadow-xl p-6 max-w-sm w-full text-center">
+                <h2 className="text-xl font-semibold text-gray-800 mb-6">
+                    Tem certeza que deseja apagar?
+                </h2>
+                <div className="flex justify-center gap-4">
+                    <button
+                        onClick={props.simClick}
+                        className={"button"}
+                    >
+                        Sim
+                    </button>
+                    <button
+                        onClick={props.naoClick}
+                        className={"button"}
+                    >
+                        Não
+                    </button>
                 </div>
-            </dialog>
-        );
-    }
-);
+            </div>
+        </dialog>
+    );
+}
 
-DeleteDialog.displayName = "DeleteDialog";
